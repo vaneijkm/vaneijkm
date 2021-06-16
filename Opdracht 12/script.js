e0bd07b68fa7062d88fdb029853bb4f08447d5d8
vaneijkm <83819557+vaneijkm@users.noreply.github.com>
2021-06-16 16:42:01 +0200

    Add files via upload

---
+++ Opdracht 12/script.js
@@ -0,0 +1,54 @@
+var age = 18 && 25;
+if (age > 18) {
+    document.write
+} ("Je krijgt 50% korting!");
+
+if (age < 25) {
+    document.write("Je krijgt 50% korting!");
+}
+else if (age < 18) {
+    document.write("geen 50% korting");
+}
+
+else if (age > 25) {
+    document.write("geen 50% korting");
+}
+
+var name = sarah || bram
+
+if (name = sarah || bram) {
+    document.write("gefeliciteerd u krijgt een gratis biertje")
+}
+
+else if (name = sarah || bram) {
+    document.write("helaas u naam is niet Sarah of Bram")
+
+}
+
+var totalamount = 25
+
+if (totalamount > 25) {
+    document.write("U krijgt gratis portie bitterballen")
+}
+else if (totalamount < 25) {
+    document.write("U heeft geen recht op een gratis portie bitterballen")
+}
+
+var totalamount = 50
+
+if (totalamount > 50) {
+    document.write("U krijgt een gratis portie Nacho's")
+}
+else if (totalamount < 50) {
+    document.write("U krijgt geen gratis portie Nacho's")
+}
+
+var totalamount = 100
+
+if (totalamount > 100) {
+    document.write("U krijgt een gratis fles Champagne")
+}
+else if (totalamount < 50) {
+    document.write("U krijgt geen gratis fles champagne")
+}
+
