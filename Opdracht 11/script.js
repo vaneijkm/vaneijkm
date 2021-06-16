dec5cc85d6ca95619aa3ed53364fd99cd9e8ae99
vaneijkm <83819557+vaneijkm@users.noreply.github.com>
2021-06-16 12:34:20 +0200

    Add files via upload

---
+++ Opdracht 11/script.js
@@ -0,0 +1,14 @@
+var isfemale = 18;
+if (isfemale > 18) {
+    document.write("je bent ouder dan 18");
+}
+else (isfemale < 18) {
+    document.write("Jen jonger dan 18");
+}
+Var driverStatus=nee;
+
+if (driverStatus = nee);
+document.write("Je mag rijden");
+else (driverStatus=ja) {
+    document.write("jemag neit meer rijden")
+}
