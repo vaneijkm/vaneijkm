1f1c5f2f1cd447e0f92dbd64ece7e7b762aad686
vaneijkm <83819557+vaneijkm@users.noreply.github.com>
2021-06-20 14:15:19 +0200

    Add files via upload

---
+++ Opdracht 12 - kopie/script.js
@@ -0,0 +1,17 @@
+
+const person = {
+    name: "Michel",
+    age: 50,
+
+}
+console.log(person.evaluations[7, 10, 9]);
+
+const myvalue = "7, 10, 9";
+console.log(person[myvalue]);
+
+const collor = ["groen", "blauw ", "rood"];
+console.log(collor.length)
+
+
+onsole.log("Herkomst
+kat 1: ", catBreeds[0].origin)
