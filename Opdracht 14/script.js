d9f2cab77b88553e904a2d01933766231fa79bbf
vaneijkm <83819557+vaneijkm@users.noreply.github.com>
2021-06-22 15:18:42 +0200

    Add files via upload

---
+++ Opdracht 14/script.js
@@ -0,0 +1,20 @@
+const pizza = function () {
+    console.log("maak deeg");
+    console.log("rol deeg uit");
+    console.log("tomatensaus op de pizza");
+    console.log("topping op de pizza");
+    console.log("mozzarella op de pizza");
+    console.log("pizza afbakken 5 minuten op 230 c");
+}
+
+const sushi = function () {
+    console.log("rijst afkoken");
+    console.log("rijst op zeewier bedekken")
+    console.log("garnituur op een rij leggen aan de voor kant")
+    console.log("zeewier vel oprollen met behulp van matje")
+    console.log("de rol in plakken snijden van 5 cm")
+
+}
+
+pizza();
+sushi();
