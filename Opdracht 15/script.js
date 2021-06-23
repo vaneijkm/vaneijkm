85300f3c371c042424c7ab9fd5cceaaa579e187e
vaneijkm <83819557+vaneijkm@users.noreply.github.com>
2021-06-23 12:59:27 +0200

    Add files via upload

---
+++ Opdracht 15/script.js
@@ -0,0 +1,22 @@
+const collor = function (red) {
+    console.log(red);
+}
+collor();
+
+const painting = function (green) {
+    console.log("i'm painting" + painting);
+    console.log("I'm painting" + collor)
+}
+painting();
+
+const nord = function (orange) {
+    console.log("the nord wall is" + nord);
+
+}
+const southeast = function (gray) {
+    console.log("the southeast wall is" + southeast)
+
+}
+
+nord();
+southeast();
