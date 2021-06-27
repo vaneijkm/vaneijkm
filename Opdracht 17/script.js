f00fd793dd7454ee920467abf25ab0244fb2bd9c
vaneijkm <83819557+vaneijkm@users.noreply.github.com>
2021-06-27 14:31:39 +0200

    Add files via upload

---
+++ Opdracht 17/script.js
@@ -0,0 +1,42 @@
+Function calculation1() {
+    console.log(number1, number2);
+    const add = number1 * number1;
+    const add2 = number2 * number2;
+    const total = add + add2;
+    const averageRounded = total * total;
+    return averageRounded;
+    add(9);
+    add2(17);
+
+}
+calculation1();
+
+const calculation2 = function () {
+    const add = (number1, number2);
+    const add = number1 * number1;
+    const add2 = number2 * number2;
+    const total = add + add2;
+    const averageRounded = total * total;
+    return averageRounded;
+    add(9, 17);
+
+
+}
+
+calculation2();
+
+const caculation3 = () => {
+    const add = (number1, number2);
+    const add = number1 * number1;
+    const add2 = number2 * number2;
+    const total = add + add2;
+    const averageRounded = total * total;
+    return averageRounded;
+    add(9, 17);
+
+
+}
+
+caculation3();
+
+
