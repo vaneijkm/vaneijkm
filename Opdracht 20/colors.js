e3edcf117ff18d9c6fbf8da6df97129a50fb9416
vaneijkm <83819557+vaneijkm@users.noreply.github.com>
2021-07-04 13:38:42 +0200

    Add files via upload

---
+++ Opdracht 20/colors.js
@@ -0,0 +1,17 @@
+const arraycolor = ['yellow', 'blue', 'orange'];
+arraycollor = i
+arraycolor.forEach(element => console.log(element));
+while (yellow, blue, orange);
+do {
+    System.out.println(i);
+    i++;
+}
+
+
+  }
+
+for color = ['yellow', 'blue', 'orange']; {
+
+    console.log(color.length);
+
+}
