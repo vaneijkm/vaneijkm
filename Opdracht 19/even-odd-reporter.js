93c080e0feade5dc5001d69985c4302e1050ed26
vaneijkm <83819557+vaneijkm@users.noreply.github.com>
2021-07-04 12:46:00 +0200

    Add files via upload

---
+++ Opdracht 19/even-odd-reporter.js
@@ -0,0 +1,12 @@
+for (let i = 0; i <= 20; i++) {
+    console.log(`Is ${i} even or odd?`);
+
+    if i = 1, 3, 5, 6, 9, 11, 13, 15, 17, 19
+
+Document.write ("This is number is odd ");
+
+if i = 2, 4, 6, 8, 10, 12, 14, 16, 18, 20
+
+Document.write ("Thus number is even");
+
+}
---
+++ Opdracht 19/multiplication-tables.js
@@ -0,0 +1,5 @@
+for ( i = a*9 ); {
+while (i * 9)
+ console.log (i * 9)
+ i++;
+}
