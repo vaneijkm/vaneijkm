cae89b16660748a045247fc52b7063452ac0cb44
vaneijkm <83819557+vaneijkm@users.noreply.github.com>
2021-07-07 14:06:50 +0200

    Add files via upload

---
+++ Opdracht 21/colors.js
@@ -0,0 +1,26 @@
+var person = prompt("Please enter your name", "naam");
+
+if (person != null) {
+  document.getElementById("demo").innerHTML =
+    "Hey " + person + "leuk je te ontmoeten";
+}
+
+var number = prompt("toets een getal tussen de 0 en 25");
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+
+
+  console.log(getRandomInt(3));
+
+  console.log(getRandomInt(1));
+
+  console.log(Math.random());
+
+}
+
+var number = match.floor("dat is niet correct")
+if (number = max) {
+  console.log(" Dit gestal is correct")
+  console.log("tot ziens", person "fijne dag");
+}
